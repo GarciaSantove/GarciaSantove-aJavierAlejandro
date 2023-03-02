8509bf69f8a1ecd84d3735d96648efc535ce5070
GarciaSantove <126818280+GarciaSantove@users.noreply.github.com>
2023-03-02 13:31:41 -0600

    termine

---
+++ GarciaSantoveñaJavierAlejandroExamen/Assets/codigo/Examen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Examen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     public class Tempo;
+    public float tiempo 0.0f;
+    public Tempo tempo; }
+
+NO PUDE HACER NADA !!!!!!!! :(
+
+
+
+        // Update is called once per frame
+        void Update()
+    {
+        tiempo -= Time.deltaTime;
+    Tempo.text = "tiempo" + " " + tiempo.ToSring("f0");  
+    }
+}
